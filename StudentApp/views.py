530c553722ca825db205b1ddf53d0bb6d7d8b0f2
IgMihai <cypryjefhardy@gmail.com>
2018-06-09 17:49:40 +0300

    WIP

--- StudentApp/views.py
+++ StudentApp/views.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import messages
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, render_to_response
 # Create your views here.
 from django.views.generic import FormView, TemplateView, ListView
-
 from StudentApp.forms import StudentForm
 from StudentApp.models import Student
+from StudentApp.models import Facultate
+from StudentApp.models import Specializare
 
 
 class RegisterStudent(FormView):
@@ -26,5 +26,50 @@ class ViewStudents(ListView):
     model = Student
 
     def get_queryset(self):
+        print("ViewStudents")
         query = Student.objects.all()
-        return query
+        facs = Facultate.objects.all()
+        specs = Specializare.objects.all()
+        obj = {'students': query, 'facs': facs, 'specs': specs}
+        return obj
+
+
+class ViewStudentsQueryFacuty(ListView):
+    template_name = "view_students.html"
+    model = Student
+
+    def get_queryset(self):
+        query = Student.objects.filter(faculty=self.kwargs['facultate_id'])
+        facs = Facultate.objects.all()
+        specs = Specializare.objects.all()
+        obj = {'students': query, 'facs': facs, 'specs': specs}
+        return obj
+
+
+class ViewStudentsQueryScec(ListView):
+    template_name = "view_students.html"
+    model = Student
+
+    def get_queryset(self):
+        a = self.request.GET
+        print(a)
+
+
+        query = Student.objects.filter(specializations=self.kwargs['specialization_id'])
+        facs = Facultate.objects.all()
+        specs = Specializare.objects.all()
+        obj = {'students': query, 'facs': facs, 'specs': specs}
+        return obj
+
+
+class ViewStudentsQueryAll(ListView):
+    template_name = "view_students.html"
+    model = Student
+
+    def get_queryset(self):
+        query = Student.objects.filter(faculty=self.kwargs['facultate_id'],
+                                       specialization=self.kwargs['specialization_id'])
+        facs = Facultate.objects.all()
+        specs = Specializare.objects.all()
+        obj = {'students': query, 'facs': facs, 'specs': specs}
+        return obj
